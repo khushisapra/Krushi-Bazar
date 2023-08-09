cc2035276c0d99c912027e3f57420672498ee846
unknown <khushisapra5300@gmail.com>
2023-08-09 13:37:07 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,228 @@
+<?php require('top.php')?>
+<div class="body__overlay"></div>
+        
+        <!-- Start Slider Area -->
+        <div class="slider__container slider--one bg__cat--3">
+            <div class="slide__container slider__activation__wrap owl-carousel">
+                <!-- Start Single Slide -->
+                <div class="single__slide animation__style01 slider__fixed--height">
+                    <div class="container">
+                        <div class="row align-items__center">
+                            <div class="col-md-7 col-sm-7 col-xs-12 col-lg-6">
+                                <div class="slide">
+                                    <div class="slider__inner">
+                                        <h2>collection 2022</h2>
+                                        <h1>AGRI TOOLS</h1>
+                                        <div class="cr__btn">
+                                            <a href="cart.html">Shop Now</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-lg-6 col-sm-5 col-xs-12 col-md-5">
+                                <div class="slide__thumb">
+                                    <img src="images/slider/fornt-img/resize-1669696682381374008dabanggcultivator (1).png" alt="slider images">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- End Single Slide -->
+                <!-- Start Single Slide -->
+                <div class="single__slide animation__style01 slider__fixed--height">
+                    <div class="container">
+                        <div class="row align-items__center">
+                            <div class="col-md-7 col-sm-7 col-xs-12 col-lg-6">
+                                <div class="slide">
+                                    <div class="slider__inner">
+                                        <h2>collection 2022</h2>
+                                        <h1>AGRI TOOLS</h1>
+                                        <div class="cr__btn">
+                                            <a href="cart.html">Shop Now</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-lg-6 col-sm-5 col-xs-12 col-md-5">
+                                <div class="slide__thumb">
+                                    <img src="images/slider/fornt-img/resize-1669696682381374008dabanggcultivator (1).png" alt="slider images">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- End Single Slide -->
+            </div>
+        </div>
+        <!-- Start Slider Area -->
+        <!-- Start Category Area -->
+        <section class="htc__category__area ptb--100">
+            <div class="container">
+                <div class="row">
+                    <div class="col-xs-12">
+                        <div class="section__title--2 text-center">
+                            <h2 class="title__line">New Arrivals</h2>
+                            <p>But I must explain to you how all this mistaken idea</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="htc__product__container">
+                    <div class="row">
+                        <div class="product__list clearfix mt--30">
+							<?php
+							$get_product=get_product($con,4);
+							foreach($get_product as $list){
+							?>
+                            <!-- Start Single Category -->
+                            <div class="col-md-4 col-lg-3 col-sm-4 col-xs-12">
+                                <div class="category">
+                                    <div class="ht__cat__thumb">
+                                        <a href="product.php?id=<?php echo $list['id']?>">
+                                            <img src="<?php echo PRODUCT_IMAGE_SITE_PATH.$list['image']?>" alt="product images">
+                                        </a>
+                                    </div>
+                                    
+                                    <div class="fr__product__inner">
+                                        <h4><a href="product-details.html"><?php echo $list['name']?></a></h4>
+                                        <ul class="fr__pro__prize">
+                                            <li class="old__prize"><?php echo $list['mrp']?></li>
+                                            <li><?php echo $list['price']?></li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- End Single Category -->
+							<?php } ?>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- End Category Area -->
+        <!-- Start Product Area -->
+        <section class="ftr__product__area ptb--100">
+            <div class="container">
+                <div class="row">
+                    <div class="col-xs-12">
+                        <div class="section__title--2 text-center">
+                            <h2 class="title__line">Best Seller</h2>
+                            <p>But I must explain to you how all this mistaken idea</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="product__wrap clearfix">
+                        <!-- Start Single Category -->
+                        <div class="col-md-4 col-lg-3 col-sm-4 col-xs-12">
+                            <div class="category">
+                                <div class="ht__cat__thumb">
+                                    <a href="product-details.html">
+                                        <img src="images\product\resize-1672294368959000706freshwheatcrop500x500.jpg" alt="product images">
+                                    </a>
+                                </div>
+                                <div class="fr__hover__info">
+                                    <ul class="product__action">
+                                        <li><a href="wishlist.html"><i class="icon-heart icons"></i></a></li>
+
+                                        <li><a href="cart.html"><i class="icon-handbag icons"></i></a></li>
+
+                                        <li><a href="#"><i class="icon-shuffle icons"></i></a></li>
+                                    </ul>
+                                </div>
+                                <div class="fr__product__inner">
+                                    <h4><a href="product-details.html"> Wheat(20kg) </a></h4>
+                                    <ul class="fr__pro__prize">
+                                        <li class="old__prize">800rs</li>
+                                        <li>850rs</li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- End Single Category -->
+                        <!-- Start Single Category -->
+                        <div class="col-md-4 col-lg-3 col-sm-4 col-xs-12">
+                            <div class="category">
+                                <div class="ht__cat__thumb">
+                                    <a href="product-details.html">
+                                        <img src="images/product/resize-16696957201352949119wickerbasketwhitebackgroundisolatedwoodenroundhandlefrontview198441686.jpg" alt="product images">
+                                    </a>
+                                </div>
+                                <div class="fr__hover__info">
+                                    <ul class="product__action">
+                                        <li><a href="wishlist.html"><i class="icon-heart icons"></i></a></li>
+
+                                        <li><a href="cart.html"><i class="icon-handbag icons"></i></a></li>
+
+                                        <li><a href="#"><i class="icon-shuffle icons"></i></a></li>
+                                    </ul>
+                                </div>
+                                <div class="fr__product__inner">
+                                    <h4><a href="product-details.html">special wood basket</a></h4>
+                                    <ul class="fr__pro__prize">
+                                        <li class="old__prize">200rs</li>
+                                        <li>250rs</li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- End Single Category -->
+                        <!-- Start Single Category -->
+                        <div class="col-md-4 col-lg-3 col-sm-4 col-xs-12">
+                            <div class="category">
+                                <div class="ht__cat__thumb">
+                                    <a href="product-details.html">
+                                        <img src="images/product/resize-1669695182165692885ed308174d902ca3bfa609c30f67006e1112620223521.jpg" alt="product images">
+                                    </a>
+                                </div>
+                                <div class="fr__hover__info">
+                                    <ul class="product__action">
+                                        <li><a href="wishlist.html"><i class="icon-heart icons"></i></a></li>
+
+                                        <li><a href="cart.html"><i class="icon-handbag icons"></i></a></li>
+
+                                        <li><a href="#"><i class="icon-shuffle icons"></i></a></li>
+                                    </ul>
+                                </div>
+                                <div class="fr__product__inner">
+                                    <h4><a href="product-details.html">sunflower seed(500kg)</a></h4>
+                                    <ul class="fr__pro__prize">
+                                        <li class="old__prize"></li>
+                                        <li>199rs</li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- End Single Category -->
+                        <!-- Start Single Category -->
+                        <div class="col-md-4 col-lg-3 col-sm-4 col-xs-12">
+                            <div class="category">
+                                <div class="ht__cat__thumb">
+                                    <a href="product-details.html">
+                                        <img src="images/product/resize-1669694784411080818ROUNDUPHERBICIDEd3e7c96c011946de89e92a7550efb117800x (2).jpg" alt="product images">
+                                    </a>
+                                </div>
+                                <div class="fr__hover__info">
+                                    <ul class="product__action">
+                                        <li><a href="wishlist.html"><i class="icon-heart icons"></i></a></li>
+
+                                        <li><a href="cart.html"><i class="icon-handbag icons"></i></a></li>
+
+                                        <li><a href="#"><i class="icon-shuffle icons"></i></a></li>
+                                    </ul>
+                                </div>
+                                <div class="fr__product__inner">
+                                    <h4><a href="product-details.html">roundup herbicide(250 ml) </a></h4>
+                                    <ul class="fr__pro__prize">
+                                        <li class="old__prize">215rs</li>
+                                        <li>215rs</li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- End Single Category -->
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- End Product Area -->
+<?php require('footer.php')?>        
